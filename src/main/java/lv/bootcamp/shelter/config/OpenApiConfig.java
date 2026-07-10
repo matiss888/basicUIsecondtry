e2b30d98889d3a3fa0ce636a1226d76a4e19650c
Matiss <matissciss@gmail.com>
2026-07-10 15:17:29 +0300

    Changed the 1 line necessary and mvn compile runs

--- src/main/java/lv/bootcamp/shelter/config/OpenApiConfig.java
+++ src/main/java/lv/bootcamp/shelter/config/OpenApiConfig.java
@@ -1,7 +1,7 @@
 package lv.bootcamp.shelter.config;
 
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
